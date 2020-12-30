fe81cd767ef24f3544e8b6df7e34371d92870be7
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-30 17:20:17 -0300

    Implementando a Lógica

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="calculator">
-    <Display value="1000" />
+    <Display :value="displayValue" />
     <Button label="AC" triple @onClick="clearMemory" />
     <Button label="/" operation @onClick="setOperation" />
     <Button label="7" @onClick="addDigit" />
-    <Button label="8" @onClick="addDigit"/>
-    <Button label="9" @onClick="addDigit"/>
-    <Button label="*" operation @onClick="setOperation"/>
-    <Button label="4" @onClick="addDigit"/>
-    <Button label="5" @onClick="addDigit"/>
-    <Button label="6" @onClick="addDigit"/>
-    <Button label="-" operation @onClick="setOperation"/>
-    <Button label="1" @onClick="addDigit"/>
-    <Button label="2" @onClick="addDigit"/>
-    <Button label="3" @onClick="addDigit"/>
-    <Button label="+" operation @onClick="setOperation"/>
-    <Button label="0" double @onClick="addDigit"/>
-    <Button label="." @onClick="addDigit"/>
-    <Button label="=" operation @onClick="setOperation"/>
+    <Button label="8" @onClick="addDigit" />
+    <Button label="9" @onClick="addDigit" />
+    <Button label="*" operation @onClick="setOperation" />
+    <Button label="4" @onClick="addDigit" />
+    <Button label="5" @onClick="addDigit" />
+    <Button label="6" @onClick="addDigit" />
+    <Button label="-" operation @onClick="setOperation" />
+    <Button label="1" @onClick="addDigit" />
+    <Button label="2" @onClick="addDigit" />
+    <Button label="3" @onClick="addDigit" />
+    <Button label="+" operation @onClick="setOperation" />
+    <Button label="0" double @onClick="addDigit" />
+    <Button label="." @onClick="addDigit" />
+    <Button label="=" operation @onClick="setOperation" />
   </div>
 </template>
 
@@ -26,15 +26,55 @@ import Display from "../components/Display.vue";
 import Button from "../components/Button.vue";
 export default {
   components: { Display, Button },
+  data() {
+    return {
+      displayValue: "0",
+      clearDisplay: false,
+      operation: null,
+      values: [0, 0],
+      current: 0
+    };
+  },
   methods: {
     clearMemory() {
-      console.log("Limpar Memória");
+      Object.assign(this.$data, this.$options.data());
     },
     setOperation(operation) {
-      console.log("Operação " + operation);
+      if(this.current === 0){
+          this.operation = operation;
+          this.current = 1;
+          this.clearDisplay = true;
+      }else{
+          const equals = operation === "=";
+          const currentOperation = this.operation;
+          try{
+              this.values[0] = eval(
+                  `${this.values[0]} ${currentOperation} ${this.values[1]}`
+              )
+          }catch(e){
+              this.$emit('onError', e);
+          }
+          this.values[1] = 0;
+          this.displayValue = this.values[0];
+          this.operation = equals ? null : operation;
+          this.current = equals ? 0 : 1;
+          this.clearDisplay = !equals;
+      }
     },
     addDigit(n) {
-      console.log("Digito " + n);
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
+      }
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
+      this.displayValue = displayValue;
+
+      if (n !== ".") {
+        const i = this.current;
+        const newValue = parseFloat(displayValue);
+        this.values[i] = newValue;
+      }
     }
   }
 };
