1b11501bb5adf3ae23631342d5cdd213851e0a71
soapmactavish23 <ricknogueira1231@gmail.com>
2020-12-30 17:07:48 -0300

    componente botão 2

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="button" :class="{ double, triple, operation }">
+  <button
+    class="button"
+    @click="$emit('onClick', label)"
+    :class="{ double, triple, operation }"
+  >
     {{ label }}
   </button>
 </template>
